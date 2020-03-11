989364a0053ea399c90d7c30c98188ad256a1e25
Oskar <oskar.case@gmail.com>
2020-03-11 11:59:32 +0200

    Edited static and media root path, removed duplicate static files

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -145,13 +145,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
 
 
 # Tinymce
